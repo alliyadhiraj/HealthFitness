2bf6d5af85754411f9eec0bf9ef47a8653e2bb94
Alliya dhiraj <alliyadhirajsubhahchandani@gmail.com>
2023-01-22 14:18:50 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".home-slider",{
+    spaceBerween: 20,
+    effect: fade,
+    loop: true,
+    grapCursor: true,
+    centeredSlider: true,
+    pagination: {
+    
+    }
+
+});
